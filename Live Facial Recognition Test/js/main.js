890be388c32506ab9d6de3f9396d200a25b6c13a
Calvin Dong <calvin.dong156@gmail.com>
2020-08-11 00:20:29 +1000

    -added traning

--- Live Facial Recognition Test/js/main.js
+++ Live Facial Recognition Test/js/main.js
@@ -3,6 +3,7 @@
 //import * as faceapi from 'face-api.js'; 
 
 const video = document.getElementById('video')
+//const picture = document.getElementById('myFace')
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('\models'),
@@ -21,13 +22,20 @@ function startVideo() {
         stream => video.srcObject = stream,
         err => console.error(err)
     )
-    console.log("hey")
     console.log(faceapi.nets)
 }
 
+async function getReferences() {
+    const results = await faceapi
+        .detectAllFaces(referenceImage)
+        .withFaceLandmarks()
+        .withFaceDescriptors()
+}
+
 video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
+    //const canvas = faceapi.createCanvasFromMedia(video)
+    //document.body.append(canvas)
+    const canvas = document.getElementById('overlay')
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
@@ -39,4 +47,17 @@ video.addEventListener('play', () => {
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     }, 100)
 })
+
+/*async function pictureDetect() {
+    const canvas = document.getElementById('overlay')
+    const pictureSize = { width: picture.width, height: picture.height }
+    faceapi.matchDimensions(canvas, pictureSize)
+    const detections = await faceapi.detectAllFaces(picture, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
+    console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections, pictureSize)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    faceapi.draw.drawAgeAndGender(canvas, resizedDetections)
+}*/
 
